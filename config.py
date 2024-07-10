fc295cbe1f84d8f8b4c33b94548cb57577f95021
Muzeomu <muzeo.mu@gmail.com>
2024-07-10 10:29:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ APP_ID = int(os.environ.get("APP_ID", "26557370"))
 API_HASH = os.environ.get("API_HASH", "98973d24e7d3bd6abd8721ac6c83d06b")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002226833735"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002147179649"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "659124762"))
@@ -28,8 +28,8 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://anshikakh333:fO01yzmrg1Ow
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNELS = int(os.environ.get("FORCE_SUB_CHANNELS", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002194638987"))
+FORCE_SUB_CHANNELS = int(os.environ.get("FORCE_SUB_CHANNELS", "-1002212996767"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
